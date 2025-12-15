6ebc099797bce8e82eebe764f5faaf998fd160e2
Giorgia Ragnoli <gi.ravioli@gmail.com>
2025-12-15 16:13:29 +0100

    ok

--- script/script.js
+++ script/script.js
@@ -7,3 +7,20 @@ for (let i = 1; i <= 90; i++) {
 
   board.appendChild(cell);
 }
+
+function pullNumber() {
+  return Math.floor(Math.random() * 90) + 1;
+}
+
+const button = document.getElementById("pullNumber");
+
+button.addEventListener("click", function () {
+  const number = pullNumber();
+  const cells = document.querySelectorAll(".cell");
+
+  cells.forEach(function (cell) {
+    if (cell.textContent == number) {
+      cell.classList.add("extracted");
+    }
+  });
+});
